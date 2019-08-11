2f1409f0deac9e0144f7d45ca4c4b797fa033b99
Markus Oberlehner <markus.oberlehner@gmail.com>
2019-08-11 06:33:41 +0200

    Reorder by importance

--- src/utils/sfc-factory.js
+++ src/utils/sfc-factory.js
@@ -1,15 +1,15 @@
-export function makeGuard(options) {
-  return Object.assign(options, {
-    beforeCreate() {
-      throw new Error(`Do not use the default export but use the factory function instead!`);
-    },
-  });
-}
-
 export function makeFactory(options, componentFactory) {
   return (...params) => ({
     ...options,
     beforeCreate() {},
     ...componentFactory(...params),
+  });
+}
+
+export function makeGuard(options) {
+  return Object.assign(options, {
+    beforeCreate() {
+      throw new Error(`Do not use the default export but use the factory function instead!`);
+    },
   });
 }
